12a411a71317774e26ecc88c097034834b5c99be
Syed Muhammed Bilal <intergo878@gmail.com>
2021-06-06 16:27:23 +0500

    Delete .gitignore directory

--- .gitignore/db/connection.ts
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const URI = "mongodb+srv://shayan:12345@cluster0.oq61y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
-    console.log('connection successful')
-}).catch((err) => {
-    console.log(err);
-});
